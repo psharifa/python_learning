fa5888e11381a3abc27e82f4f82467033b87983a
psharifa <116259640+psharifa@users.noreply.github.com>
2022-11-09 10:17:41 +0530

    Create leap_year.py

---
+++ lists/leap_year.py
@@ -0,0 +1,26 @@
+def is_leap(year):
+    leap = False
+    if year%400==0 and year%100==0:
+        leap=True
+    elif year%4==0 and year%100!=0:
+        leap=True
+    else:
+        leap=False
+    
+    # Write your logic here
+    
+    return leap
+
+year = int(input())
+
+'''
+input:
+
+1990
+
+output:
+
+False
+
+
+'''
