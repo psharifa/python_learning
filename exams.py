d6e7fe48eebf5ac0e6e0da12de5e560605b9831b
psharifa <116259640+psharifa@users.noreply.github.com>
2022-11-30 15:18:50 +0530

    Create exams.py

---
+++ exams.py
@@ -0,0 +1,18 @@
+a=input()
+l=len(a)
+for i in range(l):
+    b=int(input())
+    print(a[b],end="")
+
+
+
+'''
+input: eimag
+        1
+        2
+        3
+        4
+        0
+output: image
+
+'''
